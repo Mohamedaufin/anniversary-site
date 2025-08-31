648cef91e27a695b74f770ccb9e7618dc85e744d
Mohamedaufin <mohamedaufin64@gmail.com>
2025-08-31 20:12:08 +0530

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ import FloatingElements from "@/components/FloatingElements"
 import TapToReveal from "@/components/TapToReveal"
 
 // Change this to your anniversary date
-const ANNIVERSARY_DATE = "2025-05-25T00:00:00"
+const ANNIVERSARY_DATE = "2025-10-05T00:00:00"
 // Change this to the date you got together
 const TOGETHER_DATE = "2023-05-25T00:00:00"
 
